e98a58c53a2456453c1cea456bf902c9788168af
Catt Symonds <catherine.symonds.thompson@gmail.com>
2021-04-17 14:40:49 -0400

    level three

--- Assets/_Scripts/LevelSwitcher.cs
+++ Assets/_Scripts/LevelSwitcher.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelSwitcher : MonoBehaviour
 {
@@ -8,6 +9,7 @@ public class LevelSwitcher : MonoBehaviour
     // this would be more extensible if it were just one function that took two game objects but
     // hooking that up to a button seems non trivial and i just want this to be over honestly
 
+    public GameObject startStuff;
     public GameObject levelOne;
     public GameObject levelTwo;
     public GameObject levelThree;
@@ -21,5 +23,10 @@ public class LevelSwitcher : MonoBehaviour
     {
         levelTwo.SetActive(false);
         levelThree.SetActive(true);
+    }
+
+    public void StartAgain()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
